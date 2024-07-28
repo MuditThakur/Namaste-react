c312378c308f6a99f459bdd59d27e253a198f1a3
MuditThakur <muditthakur10@gmail.com>
2024-07-28 08:55:18 +0530

    update

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('Hello world!');
+import React, { createElement } from "react";
+import ReactDOM from "react-dom/client";
+
+//React element
+  const heading = React.createElement("h1",{id:"heading"},"This is rendered code");
+  const root = ReactDOM.createRoot(document.getElementById("root"));
+
+  const jsxScript = <h1>This is rendered using jsx </h1>
+
+  root.render(heading)
+  root.render(jsxScript   )
