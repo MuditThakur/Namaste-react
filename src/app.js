11e43dbbc75f14ae0f77130411681739f1349dfc
MuditThakur <muditthakur10@gmail.com>
2024-07-30 10:24:43 +0530

    updates

--- src/app.js
+++ src/app.js
@@ -2,18 +2,38 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 // React element
-const heading = <h1 className="head">namaste react</h1>;
+const heading = <h1 className="head">Namaste React</h1>;
 
 // React Component 
 const HeadingComponent = () => {
   return (
     <div>
-      <h1 className="heading">
-        Namaste react component hi welcome mudit 🚀🚀
+      {heading}
+      <h1 className="welcome">
+        Namaste React component, hi welcome Mudit 🚀🚀
       </h1>
     </div>
   );
 };
+
+const Component = () => {
+  return (
+    <div>
+      <h1>
+        This is a component
+      </h1>
+    </div>
+  );
+}
+
+const ComponentComposition = () => {
+  return (
+    <div className="container">
+      <HeadingComponent />
+      <Component />
+    </div>
+  );
+}
 
 // Render the elements
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -21,5 +41,6 @@ root.render(
   <div>
     {heading}
     <HeadingComponent />
+    <ComponentComposition />
   </div>
 );
