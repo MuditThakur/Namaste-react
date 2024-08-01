3cc666dd53065661b8e2683f4a173bc9e75fb49e
MuditThakur <muditthakur10@gmail.com>
2024-08-01 23:17:20 +0530

    updates

--- src/app.js
+++ src/app.js
@@ -1,46 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-// React element
-const heading = <h1 className="head">Namaste React</h1>;
-
-// React Component 
-const HeadingComponent = () => {
-  return (
-    <div>
-      {heading}
-      <h1 className="welcome">
-        Namaste React component, hi welcome Mudit 🚀🚀
-      </h1>
-    </div>
-  );
-};
-
-const Component = () => {
-  return (
-    <div>
-      <h1>
-        This is a component
-      </h1>
+const Header =()=>{
+  return(
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src="https://e7.pngegg.com/pngimages/815/847/png-clipart-take-out-android-sindelantal-app-food-text.png"/>
+      </div>
+      <div className="navItems">
+        <ul>
+          <li>Home</li>
+          <li>About us</li>
+          <li>contact us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
     </div>
   );
 }
 
-const ComponentComposition = () => {
-  return (
-    <div className="container">
-      <HeadingComponent />
-      <Component />
-    </div>
-  );
-}
 
-// Render the elements
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div>
-    {heading}
-    <HeadingComponent />
-    <ComponentComposition />
-  </div>
+const AppLayout = ()=>{
+return(
+  <div className="app">
+  <Header/>
+</div>
 );
+} 
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(<AppLayout/>)
